329c50cbe8196be6e223de76651254e27df8dcec
scp-scit <161034015+scp-scit@users.noreply.github.com>
2024-08-21 13:24:56 -0400

    14

--- sample_module/models/spaceship.py
+++ sample_module/models/spaceship.py
@@ -16,7 +16,7 @@ class Spaceship(models.Model):
                         string='Device Type',)
     contact_id = fields.Many2one('res.partner',   
                 string='Contact', index=True)
-    cost_per_gallon = fields.float(string="Cost Per Gallon")
+    cost_per_gallon = fields.Float(string="Cost Per Gallon")
     gallons_per_tank = fields.Integer(string="Gallons per tank")
 
     cost_per_tank = fields.Float(string = "Cost to Fill Tank", compute="_compute_cost_per_tank")
@@ -29,14 +29,19 @@ class Spaceship(models.Model):
     ship_volume = fields.Float(string = "Ship Volume", compute="_compute_ship_volume")   
 
 
-    @api.depends('cost_per_gallonm','gallongs_per_tank')
+    @api.depends('cost_per_gallon','gallongs_per_tank')
     def _compute_cost_per_tank(self):
-            Spaceship.cost_per_tank = Spaceship.cost_per_gallon * Spaceship.gallons_per_tank
+        for spaceship in self: 
+            spaceship.cost_per_tank = spaceship.cost_per_gallon * spaceship.gallons_per_tank
     
+                
+        
+    @api.depends('ship_height','ship_width','ship_length')
     def _compute_ship_volume(self):
-            Spaceship._compute_ship_volume = Spaceship.ship_height * Spaceship.ship_length * Spaceship.ship_width
+        for spaceship in self: 
+            spaceship._compute_ship_volume = spaceship.ship_height * spaceship.ship_length * spaceship.ship_width
 
-            
+
 
     
 
