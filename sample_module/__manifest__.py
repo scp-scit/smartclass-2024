039827f0cc34a8cc7c83a00e56d5c3af7e0f4a49
adam russell <adru@odoo.com>
2024-08-21 08:11:54 -0400

    [ADD] Sample Module

---
+++ sample_module/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Odoo Space App',
+    'version': '0.1',
+    'summary': 'App to track and manage Spaceships',
+    'author': 'Odoo',
+    'website': 'odoo.com',
+    'category': 'Parent Category/Category',
+    'license': 'OPL-1',
+
+    'depends': ['base'],
+    'data': [
+        'views/menuitems.xml',
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ sample_module/models/spaceship.py
@@ -0,0 +1,11 @@
+from odoo import api, fields, models
+
+
+class Spaceship(models.Model):
+    """Model used to store information about spaceships"""
+    _name = 'space.spaceship'
+    _description = 'Spaceship'
+
+    name = fields.Char(string="name")
+    manufacture_date = fields.Date(string="Date")
+    number = fields.Integer(string="Number")
