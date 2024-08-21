5c7e4027969a4257a8502ae55c521e3ebf438b69
scp-scit <161034015+scp-scit@users.noreply.github.com>
2024-08-21 14:31:09 -0400

    gfdgd

--- sample_module/models/partner.py
+++ sample_module/models/partner.py
@@ -3,5 +3,5 @@ from odoo import api, fields, models
 class ResPartner(models.Model):
     _inherit = 'res_partner'
 
-    spaceship_id = fields.Many2One(comodel_name='space.spaceship',string="Spaceship")
+    spaceship_id = fields.Many2one(comodel_name='space.spaceship',string="Spaceship")
                                    
